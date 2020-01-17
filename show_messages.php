2b093b3c14850b306f2908d050c8db9b3330d50f
CataAndrei97 <43951648+CataAndrei97@users.noreply.github.com>
2020-01-17 13:40:44 +0200

    Update show_messages.php

--- show_messages.php
+++ show_messages.php
@@ -3,10 +3,10 @@
 	include('db_connection.php');
 	$sql = "SELECT * FROM posts";
 	$params = array();
-	
-	$res = sqlsrv_query($conn, $sql, $params);
+	$options =  array( "Scrollable" => SQLSRV_CURSOR_KEYSET );
+	$stmt = sqlsrv_query( $conn, $sql , $params, $options );
 
-	$row_count = sqlsrv_num_rows( $res );
+	$row_count = sqlsrv_num_rows( $stmt );
 	
 	if($row_count) {
 		while($row = sqlsrv_fetch_array( $res)) {
