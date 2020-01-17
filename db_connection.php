2b9237bdd3d39524017b862ecb165c44db01ee78
CataAndrei97 <43951648+CataAndrei97@users.noreply.github.com>
2020-01-17 12:11:24 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,11 +1,20 @@
 <?php
 	include('log_fct.php');
-	$dbhost = "mysqlserverwebapp.database.windows.net";
-	$dbuser = "sql";
-	$dbpass = "Aa12345678";
-	$db = "twdatabase";
 
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $db);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:mysqlserverwebapp.database.windows.net,1433; Database = twdatabase", "sql", "Aa12345678");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "sql", "pwd" => "{your_password_here}", "Database" => "twdatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:mysqlserverwebapp.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 	if (!$conn) {
 		log_file("Connection failed: " . mysqli_connect_error());
