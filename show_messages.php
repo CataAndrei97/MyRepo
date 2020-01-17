cb818824ef9e5f1f663fb7117b22bf24ad4b8ca9
CataAndrei97 <43951648+CataAndrei97@users.noreply.github.com>
2020-01-17 12:38:25 +0200

    Update show_messages.php

--- show_messages.php
+++ show_messages.php
@@ -3,10 +3,12 @@
 	include('db_connection.php');
 	$sql = "SELECT * FROM posts";
 	
-	$res = $conn->query($sql);
+	$res = sqlsrv_query($sql);
+
+	$row_count = sqlsrv_num_rows( $res );
 	
-	if($res->num_rows > 0) {
-		while($row = $res->fetch_assoc()) {
+	if($row_count) {
+		while($row = sqlsrv_fetch_array( $res)) {
 			$_SESSION["message"] = $row["date"]. " : " . $row["name"]. " " ." say: " . $row["msg"]."<br>";
 			echo $_SESSION["message"];
 		}
